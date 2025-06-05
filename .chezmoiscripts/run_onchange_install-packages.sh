503ceb51ace7f915a6b778d8526193ce6e537b35
Ben Willemsen <ben.willemsen@anz.com>
2025-06-05 14:40:11 +1000

    Add antigen

--- .chezmoiscripts/run_onchange_install-packages.sh
+++ .chezmoiscripts/run_onchange_install-packages.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-# Ensure powerlevel10k is installed
-if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ] ; then
-  echo "Cloning powerlevel10k theme for Oh My Zsh..."
-
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+# if ~/antigen.zsh does not exists or is empty, download it
+if [ ! -f ~/antigen.zsh ] || [ ! -s ~/antigen.zsh ]; then
+    echo "Downloading antigen..."
+    curl -L git.io/antigen > ~/antigen.zsh
 fi
+
+# # Ensure powerlevel10k is installed
+# if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ] ; then
+#   echo "Cloning powerlevel10k theme for Oh My Zsh..."
+#   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+# fi
+
